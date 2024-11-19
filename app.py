9cdfe29859ca50681bb214c20899e73d7ae5dabc
khoa208 <khoadodev@gmail.com>
2024-11-18 22:08:01 -0800

    update app using environment port variable

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
-import pickle
+import pickle, os
 
 # Load the trained model and scaler
 model = pickle.load(open('model.pkl', 'rb'))
@@ -49,4 +49,5 @@ def predict():
         return jsonify({'error': str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
